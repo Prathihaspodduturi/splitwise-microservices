0c67bb2144290a18fa3c6e2b7c8290b35ae7edc8
Prathihas <prathihasreddy88@gmail.com>
2024-09-11 18:42:41 -0700

    created OtpDTO to recieve data from the backend service

---
+++ email/src/main/java/com/prathihassplitwise/email/dto/OtpDTO.java
@@ -0,0 +1,30 @@
+package com.prathihassplitwise.email.dto;
+
+public class OtpDTO {
+
+    public String email;
+
+    public String Otp;
+
+    public OtpDTO(String email, String otp)
+    {
+        this.email = email;
+        this.Otp = otp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOtp() {
+        return Otp;
+    }
+
+    public void setOtp(String otp) {
+        Otp = otp;
+    }
+}
