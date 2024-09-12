60a7ebd6bdae22e8ece87e189d56715349874a7d
Prathihas <prathihasreddy88@gmail.com>
2024-09-11 18:16:55 -0700

    Updated the code by adding sending the otp to email service

--- PrathihasSplitwiseBackend/src/main/java/com/PrathihasProjects/PrathihasSplitwise/Controller/OtpController.java
+++ PrathihasSplitwiseBackend/src/main/java/com/PrathihasProjects/PrathihasSplitwise/Controller/OtpController.java
@@ -1,6 +1,7 @@
 package com.PrathihasProjects.PrathihasSplitwise.Controller;
 
 import com.PrathihasProjects.PrathihasSplitwise.dao.UserDAOImpl;
+import com.PrathihasProjects.PrathihasSplitwise.dto.OtpDTO;
 import com.PrathihasProjects.PrathihasSplitwise.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,11 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.SecureRandom;
 
 @RestController
 @CrossOrigin
@@ -46,6 +48,9 @@ public class OtpController {
                     return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The Username or Email is not linked to any account");
             }
 
+            String otp = generateOtp();
+            sendOtp(email, otp);
+
             return ResponseEntity.ok("Otp sent successfully to email");
         }
         catch(Exception e)
@@ -54,5 +59,19 @@ public class OtpController {
         }
     }
 
+
+    private void sendOtp(String email, String otp)
+    {
+        OtpDTO otpDTO = new OtpDTO(email, otp);
+        log.info("sending communication request for the details: {}", email);
+        var result = streamBridge.send("sendOtp-out-0", email);
+        log.info("Is the communication request successfully processed?: {}", result);
+    }
+
+    public String generateOtp() {
+        SecureRandom random = new SecureRandom();
+        int otp = 100000 + random.nextInt(900000);  // Generate a number between 100000 and 999999
+        return String.valueOf(otp);  // Convert the integer OTP to a String
+    }
 
 }
